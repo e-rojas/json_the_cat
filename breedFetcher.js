25d6a828e7d168e7451ebcb1c51fd3841f088d51
javascript01fan <ejrojas@icloud.com>
2019-08-29 14:23:45 -0400

    mocha test added with refactored function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,44 +1,28 @@
-const request = require("request");
-const arg = process.argv.slice(2);
-const searchArgQuery = arg[0];
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${searchArgQuery}`,
-  (err, res) => {
-    try {
-      const data = JSON.parse(res.body);
-
-      if (data.length === 0) {
-        throw new Error("Your data returns Empty Array");
-      }
-      console.log("description", data[0].description);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-);
-/* 
 const request = require('request');
+ const  fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, resp, body) => {
+    // console.log(body);
+    // console.log(typeof body);
 
-request(url, (error, resp, body) => {
-  // console.log(body);
-  // console.log(typeof body);
+    if (error) {
+      callback(error, null);
+      // return console.log('Failed to request details: ', error);
+    }
 
-  if (error) {
-    return console.log('Failed to request details: ', error);
-  }
+    const data = JSON.parse(body);
+    // console.log(data);
 
-  const data = JSON.parse(body);
-  // console.log(data);
+    const breed = data[0]
+    if (breed) {
+      callback(null, breed.description.trim());
+      //console.log(breed.description);
+    } else {
+      callback(`Failed to find breed ${breedName}`, null);
+      //console.log(`Failed to find breed ${breedName}`);
+    }
+  });
+};
 
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-});
-*/
+module.exports = { fetchBreedDescription }
